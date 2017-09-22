efdff62044e6d924a8d5eeaa741361bb29e41a6d
root <root@clL02-5>
2017-09-22 11:47:47 +0200

    1ª versión de la clase coordenada

---
+++ src/modelo/Coordenada.java
@@ -0,0 +1,61 @@
+/**
+ * @author Daniel Díaz Lajara 77634861V
+ * @date 
+ * @param explica parámetros
+ * @return qué devuelve la función 
+ */
+package modelo;
+
+public class Coordenada {
+	private int x;
+	private int y;
+	private static int NUMERO_COORDENADAS;
+	
+	public Coordenada(int x,int y) {
+		this.x=x;
+		this.y=y;
+	}
+	public Coordenada(Coordenada otra) {
+		x=otra.x;
+		y=otra.y;
+	}
+	public static int getNumeroCoordenadas() {
+		return NUMERO_COORDENADAS;
+	}
+	@Override
+	public String toString() {
+		return "Coordenada [x=" + x + ", y=" + y + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Coordenada other = (Coordenada) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public Coordenada suma(Coordenada otra) {
+		
+	}
+}
